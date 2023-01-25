f2e928813d18dd59204ba7a343c09954b5531417
denisdelaki <denisdelaki@gmail.com>
2023-01-25 12:00:53 +0300

    modified the links connecting the pages

--- src/components/ViewHistory.js
+++ src/components/ViewHistory.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function ViewHistory() {
   const [account, setAccount] = useState('');
   const [transactions, setTransactions] = useState([]);
@@ -44,6 +44,7 @@ return (
     </ul>
 </label>
 </form>
+<Link to='/' class='back'>Back</Link>
 </div>
 
 );
